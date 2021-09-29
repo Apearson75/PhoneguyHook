1f61c7c58038bccd6596536bd59ec74e59f681e9
Phoneguytech75 <88546046+Phoneguytech75@users.noreply.github.com>
2021-09-29 15:24:12 +0000

    something sudo

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ var text = readline.question('Whatever you want?');
 hook.send(text);
 }
 
+function sudo() {
+var text = readline.question('What do you want to say?)> ')
+var user = readline.question('Type the username> ')
+hook.setUsername(user)
+hook.send(text);
+}
+
 function start() {
-var question = readline.question('Choose an option (send)>')
+var question = readline.question('Choose an option (send, sudo)>')
 if(question=='send'){
   send()
+} else if (question=='sudo'){
+  sudo()
 }
 }
 
